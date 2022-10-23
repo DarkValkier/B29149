b36df6f45620d8548b049389fee34be945f0c54b
DarkValkier <darkvalkier@gmail.com>
2022-10-23 09:39:17 +0300

    Добавлен вопрос про области

--- quiz.py
+++ quiz.py
@@ -10,3 +10,16 @@ if answer == 'в':
     print('Верно!')
 else:
     print('Неверно!')
+
+print('Из скольки областей состоит Украина?')
+print('а) 23')
+print('б) 25')
+print('в) 19')
+print('г) 20')
+
+answer = input('Ответ: ')
+
+if answer == 'б':
+    print('Верно!')
+else:
+    print('Неверно!')
