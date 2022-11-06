0526ed1dbe48bf2c255691253027e3a0686125d2
DarkValkier <darkvalkier@gmail.com>
2022-11-06 09:42:47 +0200

    Добавлен цикл для битвы насмерть

--- Lesson_2/main.py
+++ Lesson_2/main.py
@@ -8,8 +8,9 @@ player1.take_damage(1)
 print(player1)
 print(player2)
 
-player1.attack(player2)
-player2.attack(player1)
+while player1.health > 0 and player2.health > 0:
+    player1.attack(player2)
+    player2.attack(player1)
 
-print(player1)
-print(player2)
+    print(player1)
+    print(player2)
