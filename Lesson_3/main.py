fe25620d32cfcad6b00a6f53df055b5ce57bc6b8
DarkValkier <darkvalkier@gmail.com>
2022-11-13 11:55:34 +0200

    Заменён код со студентом на проверку работы класса рыцаря

--- Lesson_3/main.py
+++ Lesson_3/main.py
@@ -1,5 +1,16 @@
-from student import Student
+from Lesson_2.character import Character
+from berserk import Berserk
+from knight import Knight
 
-stud1 = Student(name='Василий')
+player1 = Berserk(name='Vasya', damage=10)
+player2 = Knight(name='Petya', damage=15, defence=2)
 
-print(stud1.get_age())
+print(player1)
+print(player2)
+
+while player1.is_alive() and player2.is_alive():
+    player1.attack(player2)
+    player2.attack(player1)
+
+    print(player1)
+    print(player2)
